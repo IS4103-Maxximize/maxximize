675061bbc9a4d23509f78e8b64e6952d676e7e8b
Jing Hao <jinghaoong@gmail.com>
2022-09-12 22:04:03 +0800

    fix: removed unused apps site-manufacturer and site-supplier and fixed scripts

--- apps/site-manufacturer/src/helpers/constants.js
+++
@@ -1,9 +0,0 @@
-const apiPort = 3000;
-const apiHost = `http://localhost:${apiPort}/api`;
-
-const headers = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-};
-
-export { apiHost, headers}
