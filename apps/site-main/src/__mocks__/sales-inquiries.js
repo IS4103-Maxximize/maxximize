c09e45c677ceda72e60529dfd6f1f306ef3a74e6
Jing Hao <jinghaoong@gmail.com>
2022-09-13 01:07:46 +0800

    chore: mock sales inquiries

---
+++ apps/site-main/src/__mocks__/sales-inquiries.js
@@ -0,0 +1,68 @@
+import { v4 as uuid } from 'uuid';
+
+export const salesInquiries = [
+  {
+    id: uuid(),
+    status: 'draft',
+    lineItems: [
+      {
+        id: 1,
+        subTotal: 10,
+        product: {
+          id: 1,
+          name: 'tomato',
+          description: 'from NZ',
+          skuCode: '1-TOM',
+          unit: 'kilogram',
+          unitPrice: 10,
+          expiry: 10,
+        }
+      },
+      {
+        id: 2,
+        subTotal: 5,
+        product: {
+          id: 2,
+          name: 'potato]',
+          description: 'from AUS',
+          skuCode: '2-POT',
+          unit: 'kilogram',
+          unitPrice: 15,
+          expiry: 9,
+        }
+      },
+    ]
+  },
+  {
+    id: uuid(),
+    status: 'pending',
+    lineItems: [
+      {
+        id: 1,
+        subTotal: 10,
+        product: {
+          id: 1,
+          name: 'tomato',
+          description: 'from NZ',
+          skuCode: '1-TOM',
+          unit: 'kilogram',
+          unitPrice: 10,
+          expiry: 10,
+        }
+      },
+      {
+        id: 2,
+        subTotal: 5,
+        product: {
+          id: 2,
+          name: 'potato]',
+          description: 'from AUS',
+          skuCode: '2-POT',
+          unit: 'kilogram',
+          unitPrice: 15,
+          expiry: 9,
+        }
+      },
+    ]
+  },
+]
