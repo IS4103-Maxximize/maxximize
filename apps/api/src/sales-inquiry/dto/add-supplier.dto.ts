20b49868aed73e10c6c430c41a68e10fb690d530
ryanyuxy <ryanyuxy@gmail.com>
2022-09-16 20:19:07 +0800

    created sales-enquiry api for sending email

--- apps/api/src/sales-inquiry/dto/add-supplier.dto.ts
+++
@@ -1,4 +0,0 @@
-export class AddSupplierDto {
-    salesInquiryId: number;
-    shellOrganisationId: number;
-}
---
+++ apps/api/src/sales-inquiry/dto/send-email.dto.ts
@@ -0,0 +1,4 @@
+export class SendEmailDto {
+    salesInquiryId: number;
+    shellOrganisationIds: number[];
+}
