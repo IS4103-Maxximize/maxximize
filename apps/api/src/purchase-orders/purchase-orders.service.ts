c853590cf7bacb7f1b73b1d26019c95c48330590
ryanyuxy <77622387+ryanyuxy@users.noreply.github.com>
2022-09-21 23:51:31 +0800

    added followuplineitems service (#86)

--- apps/api/src/purchase-orders/purchase-orders.service.ts
+++ apps/api/src/purchase-orders/purchase-orders.service.ts
@@ -101,8 +101,8 @@ export class PurchaseOrdersService {
       })
       const organisation = await this.organisationsService.findOne(currentOrganisationId)
       const supplier = (await this.quotationsService.findOne(quotationId)).shellOrganisation
-      const deliveryTime = newPurchaseOrder.deliveryDate.toLocaleDateString()
-      this.mailService.sendPurchaseOrderEmail(supplierContact.email, organisation.name, supplier.name, poLineItems, newPurchaseOrder, deliveryTime)
+      // const deliveryTime = newPurchaseOrder.deliveryDate.toLocaleDateString()
+      // this.mailService.sendPurchaseOrderEmail(supplierContact.email, organisation.name, supplier.name, poLineItems, newPurchaseOrder, deliveryTime)
       
       return newPurchaseOrder
     } catch (error) {
