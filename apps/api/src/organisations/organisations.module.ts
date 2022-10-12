4d523ed364effb2a0d19ce0adf285c3aeef89e4e
Bennyphoe <48516318+Bennyphoe@users.noreply.github.com>
2022-10-12 18:40:19 +0800

    registering Org and user service update (#150)

--- apps/api/src/organisations/organisations.module.ts
+++ apps/api/src/organisations/organisations.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Billing } from '../billings/entities/billing.entity';
 import { Contact } from '../contacts/entities/contact.entity';
+import { MailModule } from '../mail/mail.module';
 import { Order } from '../orders/entities/order.entity';
 import { PurchaseOrder } from '../purchase-orders/entities/purchase-order.entity';
 import { SalesInquiry } from '../sales-inquiry/entities/sales-inquiry.entity';
 import { ShellOrganisation } from '../shell-organisations/entities/shell-organisation.entity';
 import { ShellOrganisationsModule } from '../shell-organisations/shell-organisations.module';
 import { User } from '../users/entities/user.entity';
+import { UsersModule } from '../users/users.module';
 import { Machine } from '../vehicles/entities/vehicle.entity';
 import { Warehouse } from '../warehouses/entities/warehouse.entity';
 import { Organisation } from './entities/organisation.entity';
@@ -15,7 +17,7 @@ import { OrganisationsController } from './organisations.controller';
 import { OrganisationsService } from './organisations.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Organisation, User, Contact, Machine, Order, Warehouse, Billing, SalesInquiry, PurchaseOrder, ShellOrganisation]), ShellOrganisationsModule],
+  imports: [TypeOrmModule.forFeature([Organisation, User, Contact, Machine, Order, Warehouse, Billing, SalesInquiry, PurchaseOrder, ShellOrganisation]), ShellOrganisationsModule, forwardRef(() => UsersModule), MailModule],
   controllers: [OrganisationsController],
   providers: [OrganisationsService],
   exports: [OrganisationsService]
