733e12828b6e8ac5df0eadca84a681cf952d17f7
Goh Yong Jie <gohyongjie1112@gmail.com>
2022-10-17 14:13:47 +0800

    chore: fixed imports

--- apps/api/src/batches/batches.service.ts
+++ apps/api/src/batches/batches.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { randomUUID } from 'crypto';
 import { DataSource, QueryRunner, Repository } from 'typeorm';
