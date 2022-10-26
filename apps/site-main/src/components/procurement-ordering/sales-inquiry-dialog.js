739e9f2f2a66b94662c6b4d10e3569adb1134bac
Jing Hao <jinghaoong@gmail.com>
2022-10-26 21:37:29 +0800

    fix: added check for null receiving org ID in sales inquiry dialog

--- apps/site-main/src/components/procurement-ordering/sales-inquiry-dialog.js
+++ apps/site-main/src/components/procurement-ordering/sales-inquiry-dialog.js
@@ -117,7 +117,7 @@ export const SalesInquiryDialog = (props) => {
       const expiryDuration = new Date(values.expiryDate) - new Date();
 
       const salesInquiry = {
-        receivingOrganisationId: values.receivingOrg,
+        receivingOrganisationId: values.receivingOrg ? values.receivingOrg : '', // check for null orgId
         expiryDuration: expiryDuration,
         totalPrice: totalPrice,
         currentOrganisationId: currentOrganisationId,
