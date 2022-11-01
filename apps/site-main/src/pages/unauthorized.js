273e1be76e9cdb2a64838f77705e4da6b4e767c8
Jing Hao <jinghaoong@gmail.com>
2022-11-01 22:00:07 +0800

    feat: added home button to unauthorized page

--- apps/site-main/src/pages/unauthorized.js
+++ apps/site-main/src/pages/unauthorized.js
@@ -1,8 +1,22 @@
+import HomeIcon from '@mui/icons-material/Home';
+import { Button, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
 function Unauthorized() {
   return (
     <div className="container" style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <main className="textField">
             <h1 style={{fontSize: 30}}>You are unauthorized! Please go back.</h1>
+            <Link component={RouterLink} underline="none" to='/'>
+              <Button
+                component="a"
+                startIcon={<HomeIcon fontSize="small" />}
+                sx={{ mt: 3 }}
+                variant="contained"
+              >
+                Home
+              </Button>
+            </Link>
         </main>
     </div>
   )
