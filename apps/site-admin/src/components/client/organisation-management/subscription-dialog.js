0245efa107541c160c28b07cbfe69660e98ca9bf
JYLim99 <jiayinglim@live.com>
2022-11-08 01:07:08 +0800

    feat/Added invoices and subscriptions + invoices for org

--- apps/site-admin/src/components/client/organisation-management/subscription-dialog.js
+++ apps/site-admin/src/components/client/organisation-management/subscription-dialog.js
@@ -7,10 +7,12 @@ import {
   Toolbar,
   Typography,
 } from '@mui/material';
-import { useFormik } from 'formik';
 import CloseIcon from '@mui/icons-material/Close';
+import ViewListOutlinedIcon from '@mui/icons-material/ViewListOutlined';
 import { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import DayJS from 'dayjs';
+import { SubscriptionInvoiceDialog } from './subscription-invoice-dialog';
 
 export const SubscriptionDialog = (props) => {
   const { open, handleClose, organisation } = props;
@@ -19,62 +21,51 @@ export const SubscriptionDialog = (props) => {
   const [subscriptions, setSubscriptions] = useState([]);
 
   // Retrieve all subscriptions invoices
-  //   const retrieveAllInvoiceOfSubscriptions = async () => {
-  //     const response = await fetch(
-  //       `http://localhost:3000/api/memberships/stripe/invoices/subscriptions/${organisation?.membership?.subscriptionId}`
-  //     );
-  //     let result = [];
+  const retrieveAllSubscriptions = async () => {
+    const response = await fetch(
+      `http://localhost:3000/api/memberships/stripe/subscriptions/customers/${organisation?.membership?.customerId}`
+    );
+    let result = [];
 
-  //     if (response.status == 200 || response.status == 201) {
-  //       result = await response.json();
-  //     }
+    if (response.status == 200 || response.status == 201) {
+      result = await response.json();
+    }
 
-  //     setSubscriptions(result);
-  //   };
-
-  // Retrieve membership details
-  //   const retrieveMembership = async () => {
-  //     const response = await fetch(
-  //       `http://localhost:3000/api/memberships/orgId/${organisation.id}`
-  //     );
-
-  //     if (response.status == 200 || response.status == 201) {
-  //       const result = await response.json();
-  //       return result;
-  //     } else {
-  //       const result = await response.json();
-  //       setError(result.message);
-  //     }
-  //   };
+    setSubscriptions(result);
+  };
 
   useEffect(() => {
     if (organisation) {
-      console.log(organisation);
-      //   retrieveAllInvoiceOfSubscriptions();
+      retrieveAllSubscriptions();
     }
   }, [open]);
 
-  useEffect(() => console.log(subscriptions), [subscriptions]);
+  // Dialog helpers
+  const [subscriptionInvoicesDialogOpen, setSubscriptionInvoicesDialogOpen] =
+    useState(false);
 
-  const formik = useFormik({
-    initialValues: {
-      orgName: organisation ? organisation.name : '',
-      uen: organisation ? organisation.uen : '',
-      orgEmail: organisation ? organisation.contact.email : '',
-      orgAddress: organisation ? organisation.contact.address : '',
-      orgPostalCode: organisation ? organisation.contact.postalCode : '',
-      orgPhoneNumber: organisation ? organisation.contact.phoneNumber : '',
-    },
-    enableReinitialize: true,
-  });
-
-  // Delete Confirm dialog
-  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
-  const handleConfirmDialogOpen = () => {
-    setConfirmDialogOpen(true);
+  const handleClickOpen = () => {
+    setSubscriptionInvoicesDialogOpen(true);
   };
-  const handleConfirmDialogClose = () => {
-    setConfirmDialogOpen(false);
+
+  const handleClickClose = () => {
+    setSubscriptionInvoicesDialogOpen(false);
+  };
+
+  const [selectedRow, setSelectedRow] = useState();
+
+  // Action button
+  const actionButton = (params) => {
+    return (
+      <IconButton
+        onClick={(event) => {
+          setSelectedRow(params.row);
+          handleClickOpen();
+        }}
+      >
+        <ViewListOutlinedIcon color="primary" />
+      </IconButton>
+    );
   };
 
   // Close dialog
@@ -84,84 +75,133 @@ export const SubscriptionDialog = (props) => {
   };
 
   // Columns for subscription datagrid
-  //   const subscriptionColumns = [
-  //     {
-  //       field: 'id',
-  //       headerName: 'ID',
-  //       flex: 1,
-  //     },
-  //     {
-  //       field: 'created',
-  //       headerName: 'Created',
-  //       flex: 2,
-  //       valueGetter: (params) => {
-  //         return params.row ? params.row.created : '';
-  //       },
-  //     },
-  //     {
-  //       field: 'total',
-  //       headerName: 'Total',
-  //       flex: 4,
-  //       valueGetter: (params) => {
-  //         return params.row ? params.row.total : '';
-  //       },
-  //     },
-  //     {
-  //       field: 'status',
-  //       headerName: 'Status',
-  //       flex: 2,
-  //       valueGetter: (params) => {
-  //         return params.row ? params.row.status : '';
-  //       },
-  //     },
-  //   ];
+  const subscriptionColumns = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      flex: 3,
+    },
+    {
+      field: 'plan',
+      headerName: 'Plan',
+      flex: 1,
+      valueGetter: (params) => {
+        return params.row ? params.row.plan : '';
+      },
+    },
+    {
+      field: 'created',
+      headerName: 'Created',
+      flex: 2,
+      valueGetter: (params) => {
+        return params.row ? params.row.created : '';
+      },
+      valueFormatter: (params) =>
+        DayJS(params?.value).format('DD MMM YYYY hh:mm a'),
+    },
+    {
+      field: 'currentPeriodStart',
+      headerName: 'Current Period Start',
+      flex: 2,
+      valueGetter: (params) => {
+        return params.row ? params.row.currentPeriodStart : '';
+      },
+      valueFormatter: (params) =>
+        DayJS(params?.value).format('DD MMM YYYY hh:mm a'),
+    },
+    {
+      field: 'currentPeriodEnd',
+      headerName: 'Current Period End',
+      flex: 2,
+      valueGetter: (params) => {
+        return params.row ? params.row.currentPeriodEnd : '';
+      },
+      valueFormatter: (params) =>
+        DayJS(params?.value).format('DD MMM YYYY hh:mm a'),
+    },
+    {
+      field: 'endedAt',
+      headerName: 'Ended At',
+      flex: 2,
+      valueGetter: (params) => {
+        return params.row ? params.row.endedAt : '';
+      },
+      valueFormatter: (params) =>
+        DayJS(params?.value).format('DD MMM YYYY hh:mm a') === 'Invalid Date'
+          ? 'NA'
+          : DayJS(params?.value).format('DD MMM YYYY hh:mm a'),
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      flex: 2,
+      valueGetter: (params) => {
+        return params.row ? params.row.status : '';
+      },
+      valueFormatter: (params) => {
+        const valueFormatted =
+          params.value[0].toUpperCase() + params.value.slice(1);
+        return `${valueFormatted}`;
+      },
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      flex: 1,
+      sortable: false,
+      renderCell: actionButton,
+    },
+  ];
 
   return (
     <>
-      <form onSubmit={formik.handleSubmit}>
-        <Dialog
-          fullScreen
-          open={open}
-          onClose={onClose}
-          aria-labelledby="responsive-dialog-title"
-        >
-          <AppBar color="success" sx={{ position: 'relative' }}>
-            <Toolbar>
-              <IconButton
-                edge="start"
-                color="inherit"
-                onClick={onClose}
-                aria-label="close"
-              >
-                <CloseIcon />
-              </IconButton>
-              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                Subscriptions
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <DialogContent>
-            <Box display="flex" justifyContent={'flex-end'}>
-              <Typography variant="caption" color="red">
-                {error}
-              </Typography>
-            </Box>
-            <Box mt={2}>
-              <Typography variant="h6">Subscriptions</Typography>
-              {/* <DataGrid
-                autoHeight
-                rows={subscriptions}
-                columns={subscriptionColumns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                // onSelectionModelChange={(ids) => setSelectedRows(ids)}
-                // processRowUpdate={handleRowUpdate}
-                disableSelectionOnClick
-              /> */}
-            </Box>
-          </DialogContent>
-        </Dialog>
-      </form>
+      <SubscriptionInvoiceDialog
+        open={subscriptionInvoicesDialogOpen}
+        handleClose={handleClickClose}
+        subscription={selectedRow}
+      />
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={onClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <AppBar color="success" sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={onClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Subscriptions
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <DialogContent>
+          <Box display="flex" justifyContent={'flex-end'}>
+            <Typography variant="caption" color="red">
+              {error}
+            </Typography>
+          </Box>
+          <Box mt={2}>
+            <Typography variant="h6">Subscriptions</Typography>
+            <DataGrid
+              autoHeight
+              rows={subscriptions}
+              columns={subscriptionColumns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              // onSelectionModelChange={(ids) => setSelectedRows(ids)}
+              // processRowUpdate={handleRowUpdate}
+              disableSelectionOnClick
+            />
+          </Box>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
