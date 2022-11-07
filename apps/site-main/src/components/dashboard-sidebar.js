994b75d083b7f1ad241303a3fcfab4bbdf86f89f
ktliu135 <65388353+ktliu135@users.noreply.github.com>
2022-11-07 14:01:03 +0800

    DeliveryFleetManagement (#176)
    * delivery fleet management front end
    
    * added view delivery request
    
    * commit
    
    * fix: fixed vehicle status not updating
    
    * minor edits, removed unnecessary imports
    
    * merged delivery management to asset management
    
    * made changes to App.js
    
    * most updated version
    
    Co-authored-by: Jing Hao <jinghaoong@gmail.com>

--- apps/site-main/src/components/dashboard-sidebar.js
+++ apps/site-main/src/components/dashboard-sidebar.js
@@ -66,6 +66,7 @@ const standalone = [
     subsystemName: '',
     access: ['admin', 'superadmin'],
   },
+
 ];
 
 const items = [
@@ -225,6 +226,12 @@ const items = [
         icon: <AddRoadIcon fontSize="small" />,
         title: 'Production Line',
         access: ['manager', 'factoryworker', 'superadmin'],
+      },
+      {
+        href: '/production/delivery-fleet-management',
+        icon: <LocalShippingIcon fontSize="small" />,
+        title: 'Delivery Fleet',
+        access: [ 'manager', 'factoryworker', 'superadmin'],
       },
       {
         href: '/production/production-request',
