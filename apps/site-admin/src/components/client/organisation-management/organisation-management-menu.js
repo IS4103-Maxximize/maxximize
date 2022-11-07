a3ce4a1e0f04aac4c0f877dbb01137760f7fcdf9
JYLim99 <77625138+JYLim99@users.noreply.github.com>
2022-11-07 16:46:52 +0800

    feat/Ban and unban organisation, invoices added (#209)

--- apps/site-admin/src/components/client/organisation-management/organisation-management-menu.js
+++ apps/site-admin/src/components/client/organisation-management/organisation-management-menu.js
@@ -1,4 +1,6 @@
 import SearchIcon from '@mui/icons-material/Search';
+import DescriptionIcon from '@mui/icons-material/Description';
+import CardMembershipIcon from '@mui/icons-material/CardMembership';
 import {
   ListItemIcon,
   ListItemText,
@@ -8,7 +10,14 @@ import {
 } from '@mui/material';
 
 export const OrganisationManagementMenu = (props) => {
-  const { anchorEl, menuOpen, handleMenuClose, handleDialogOpen } = props;
+  const {
+    anchorEl,
+    menuOpen,
+    handleMenuClose,
+    handleDialogOpen,
+    handleSubscriptionDialogOpen,
+    handleInvoiceDialogOpen,
+  } = props;
   return (
     <Menu anchorEl={anchorEl} open={menuOpen} onClose={handleMenuClose}>
       <MenuList>
@@ -22,6 +31,28 @@ export const OrganisationManagementMenu = (props) => {
             <SearchIcon />
           </ListItemIcon>
           <ListItemText>View Details</ListItemText>
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleMenuClose();
+            handleSubscriptionDialogOpen();
+          }}
+        >
+          <ListItemIcon>
+            <CardMembershipIcon />
+          </ListItemIcon>
+          <ListItemText>View Subscriptions</ListItemText>
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleMenuClose();
+            handleInvoiceDialogOpen();
+          }}
+        >
+          <ListItemIcon>
+            <DescriptionIcon />
+          </ListItemIcon>
+          <ListItemText>View Invoices</ListItemText>
         </MenuItem>
       </MenuList>
     </Menu>
