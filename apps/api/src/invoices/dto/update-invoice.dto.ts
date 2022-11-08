43d3e91d129f319fb0fd30ca275e62c89f51bfdb
ryanyuxy <ryanyuxy@gmail.com>
2022-11-08 15:51:02 +0800

    paymentReceived

--- apps/api/src/invoices/dto/update-invoice.dto.ts
+++ apps/api/src/invoices/dto/update-invoice.dto.ts
@@ -3,5 +3,5 @@ import { InvoiceStatus } from '../enums/invoiceStatus.enum';
 import { CreateInvoiceDto } from './create-invoice.dto';
 
 export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {
-    status: InvoiceStatus
+    status?: InvoiceStatus
 }
