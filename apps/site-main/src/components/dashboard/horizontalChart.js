fe02e104822462718eed8a92d7069e2bce690888
Kathy <ktliu135@gmail.com>
2022-11-12 18:43:48 +0800

    dashboard chart

---
+++ apps/site-main/src/components/dashboard/horizontalChart.js
@@ -0,0 +1,153 @@
+import { Bar } from 'react-chartjs-2';
+import { Box, Button, Card, CardContent, CardHeader, Divider, TextField, useTheme} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+
+export const HorizontalChart = (props) => {
+  const{ 
+    counter,
+    graphLabels,
+    graphData,
+    graphTitle
+    }= props;
+       
+  const theme = useTheme();
+
+  
+  const data ={
+    labels: graphLabels,
+    datasets: [{
+    axis: 'y',
+    data: graphData,
+    fill: false,
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+    ],
+    borderWidth: 1
+      }]
+    };
+
+  const options = {
+    indexAxis: 'y',
+    animation: false,
+    cornerRadius: 20,
+    layout: { padding: 0 },
+    legend: { display: false },
+    maintainAspectRatio: false,
+    responsive: true,
+    xAxes: [
+      {
+        ticks: {
+          fontColor: theme.palette.text.secondary
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        }
+      }
+    ],
+    yAxes: [
+      {
+        ticks: {
+          fontColor: theme.palette.text.secondary,
+          beginAtZero: true,
+          min: 0
+        },
+        gridLines: {
+          borderDash: [2],
+          borderDashOffset: [2],
+          color: theme.palette.divider,
+          drawBorder: false,
+          zeroLineBorderDash: [2],
+          zeroLineBorderDashOffset: [2],
+          zeroLineColor: theme.palette.divider
+        }
+      }
+    ],
+    tooltips: {
+      backgroundColor: theme.palette.background.paper,
+      bodyFontColor: theme.palette.text.secondary,
+      borderColor: theme.palette.divider,
+      borderWidth: 1,
+      enabled: true,
+      footerFontColor: theme.palette.text.secondary,
+      intersect: false,
+      mode: 'index',
+      titleFontColor: theme.palette.text.primary
+    }
+  };
+
+  return (
+    <Card {...props}>
+      <CardHeader
+        title = {`${graphTitle}`}
+        action={(
+          <Box
+            width={200}
+          >
+          {counter}
+        </Box>
+        )}
+      />
+
+      <Divider />
+      <CardContent>
+        <Box
+          sx={{
+            height: 400,
+            position: 'relative'
+          }}
+        >
+          <Bar
+            data={data}
+            options={options}
+          />
+        </Box>
+      </CardContent>
+      <Divider />
+    </Card>
+  );
+};
--- apps/site-main/src/pages/dashboard.js
+++ apps/site-main/src/pages/dashboard.js
@@ -1,9 +1,11 @@
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import { Box, Button, Container, Grid, Link, Skeleton, Typography } from '@mui/material';
+import { Box, Button, Container, FormControl, Grid, InputLabel, Link, MenuItem, Select, Skeleton, TextField, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Blur } from '../components/blur';
 import { Budget } from '../components/dashboard/budget';
+import { Chart } from '../components/dashboard/chart';
+import { HorizontalChart } from '../components/dashboard/horizontalChart';
 import { LatestOrders } from '../components/dashboard/latest-orders';
 import { LatestProducts } from '../components/dashboard/latest-products';
 import { Sales } from '../components/dashboard/sales';
@@ -13,6 +15,7 @@ import { TotalProfit } from '../components/dashboard/total-profit';
 import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
 import { NotificationAlert } from '../components/notification-alert';
 import { getSessionUrl } from '../helpers/stripe';
+import { apiHost, headers, requestOptionsHelper } from '../helpers/constants';
 
 const Dashboard = () => {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -36,10 +39,163 @@ const Dashboard = () => {
     setAlertOpen(false);
   };
 
-  const workerDashboardPerms = [
-    'factoryworker',
-    'superadmin',
-  ]
+  const today = new Date();
+
+
+       // Date Helpers
+ const [selectedDays, setSelectedDays] = useState('');
+
+ useEffect(() => {
+  if (selectedDays) { 
+    getGraphData();
+  }
+}, [selectedDays])
+
+ const onChange = (event) => {
+  console.log(event.target)
+  setSelectedDays(event.target.value);
+};
+
+ // Date Helpers
+ const [selectedCount, setSelectedCount] = useState('');
+
+ useEffect(() => {
+   if (selectedCount) { 
+     getHorizontalGraphData1()
+   }
+ }, [selectedCount])
+
+
+ const handleChange = (event) => {
+   console.log(event.target)
+   setSelectedCount(event.target.value);
+ };
+
+ const DropDown = () => {
+   return(
+     <FormControl fullWidth>
+         <InputLabel id="demo-simple-select-label">Date Range</InputLabel>
+         <Select
+           labelId="demo-simple-select-label"
+           id="demo-simple-select"
+           value={selectedDays}
+           label="time range"
+           onChange={onChange}
+         >
+           <MenuItem value={"7"}>Last 7 days</MenuItem>
+           <MenuItem value={"14"}>Last 14 days</MenuItem>
+           <MenuItem value={"21"}>Last 21 days</MenuItem>
+         </Select>
+       </FormControl>
+   );
+ };
+
+   // const maxNumberOfCounts= organisationId.shellOrganisations;
+   const Counter = () => {
+    return(
+      <TextField 
+      fullWidth
+      type="number"
+      inputProps={{ max: 20, min: 0 }}
+      sx={{ width: 100 }}
+      label="Counter"
+      margin="normal"
+      name="Counter"
+      onChange={handleChange}
+      value={selectedCount}
+      />
+    );
+  };
+
+  //chart data 
+  const [graphDataThisYear, setGraphDataThisYear] = useState([]);
+  const [graphDataLastYear, setGraphDataLastYear] = useState([]);
+  const [dateLabels, setDateLabels] = useState([]);
+
+  const getGraphData = async () => {
+    const apiUrl = `${apiHost}/revenue/salesAmount`;
+    let body = {
+      days: selectedDays,
+      currentDate: today,
+      organisationId: organisationId
+    };
+    body = JSON.stringify(body);
+    const requestOptions=requestOptionsHelper('POST', body);
+    // const sub= selectedDays -1;
+   await fetch(apiUrl,requestOptions)
+    .then((response) => response.json())
+    .then(result => {
+      const arrSalesAmountThisYear = []
+      const arrSalesAmountLastYear = []
+      const arrDateLabels = []
+      result.forEach(item => {
+        arrSalesAmountThisYear.push(item.salesAmountThisYear)
+        arrSalesAmountLastYear.push(item.salesAmountLastYear)
+        arrDateLabels.push(item.dateKey)
+      })
+      const reversedSalesThisYear = arrSalesAmountThisYear.reverse();
+      const reversedSalesLastYear = arrSalesAmountLastYear.reverse();
+      const reversedDays = arrDateLabels.reverse();
+      // let fromDataDate= arrDateLabels[arrDateLabels.length-1];
+      // for (var i = 0; i < selectedDays ; i++) {
+      //   days.push(fromDataDate);
+      //   fromDataDate = Dayjs(fromDataDate).add(1, 'day');
+      // }
+      setGraphDataThisYear(reversedSalesThisYear);
+      setGraphDataLastYear(reversedSalesLastYear);
+      setDateLabels(reversedDays);
+    })
+  };
+    //horizontal graph data
+    const [graphData1, setGraphData1] = useState([]);
+    const [graphLabels1, setGraphLabels1] = useState([]);
+    const getHorizontalGraphData1 = async () => {
+    const apiUrl = `${apiHost}/revenue/contributors`;
+    let body = {
+        inDate: today,
+        start: null,
+        end: null,
+        range: false,
+        type: "month",
+        organisationId: organisationId,
+        count:selectedCount
+        };
+        body = JSON.stringify(body);
+        const requestOptions=requestOptionsHelper('POST', body);
+    await fetch(apiUrl,requestOptions)
+    .then((response) => response.json())
+    .then(result => {
+      const nameLabels = []
+      const contributionLabels = []
+
+      result.forEach(item => {
+        nameLabels.push(item.name)
+        contributionLabels.push(item.contribution)
+      })
+      setGraphData1(contributionLabels);
+      setGraphLabels1(nameLabels);
+    })
+  };
+
+          //horizontal graph data
+          const [graphData2, setGraphData2] = useState([]);
+          const [graphLabels2, setGraphLabels2] = useState([]);
+          const getHorizontalGraphData2 = async () => {
+          const apiUrl = `${apiHost}/final-goods/topSales/${organisationId}`;
+          await fetch(apiUrl)
+          .then((response) => response.json())
+          .then(result => {
+            const nameLabels = []
+            const quantityLabels = []
+            result.forEach(item => {
+              nameLabels.push(item.name)
+              quantityLabels.push(item.quantity)
+            })
+            setGraphData2(quantityLabels);
+            setGraphLabels2(nameLabels);
+          })
+        };
+
 
 
   const DefaultDashboardItems = () => (
@@ -57,8 +213,29 @@ const Dashboard = () => {
         <TotalProfit sx={{ height: '100%' }} />
       </Grid>
       <Grid item lg={8} md={12} xl={9} xs={12}>
-        <Sales />
-      </Grid>
+            <Chart
+            dropDown={<DropDown/>}
+            dateLabels={dateLabels}
+            graphDataThisYear={graphDataThisYear}
+            graphDataLastYear={graphDataLastYear}
+            graphTitle= "Total Sales Amount"/>
+          </Grid>
+          <Grid item lg={6} md={12} xl={9} xs={12}>
+        <HorizontalChart 
+        graphLabels={graphLabels2}
+        graphData={graphData2}
+        graphTitle="Top 5 best selling goods this month"
+        sx={{ height: '100%' }} />
+      </Grid> 
+      <Grid item lg={6} md={12} xl={9} xs={12}>
+        <HorizontalChart 
+            counter={<Counter/>}
+            graphLabels={graphLabels1}
+            graphData={graphData1}
+            graphTitle={`Top ${selectedCount} customers contributing to revenue`}
+            sx={{ height: '100%' }}
+             />
+      </Grid> 
       <Grid item lg={4} md={6} xl={3} xs={12}>
         <TrafficByDevice sx={{ height: '100%' }} />
       </Grid>
