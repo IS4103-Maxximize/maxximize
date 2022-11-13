38c12c55662aebba8b7e0b083906eb717328e60a
Jing Hao <jinghaoong@gmail.com>
2022-11-13 10:50:47 +0800

    feat: added charts, co-authored by Kathy

--- apps/site-main/src/pages/dashboard.js
+++ apps/site-main/src/pages/dashboard.js
@@ -2,12 +2,14 @@ import GroupsIcon from '@mui/icons-material/Groups';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import MoneyIcon from '@mui/icons-material/Money';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
-import { Box, Button, Container, Grid, Link, Skeleton, Typography } from '@mui/material';
+import { Box, Button, Container, FormControl, Grid, InputLabel, Link, MenuItem, Select, Skeleton, TextField, Typography } from '@mui/material';
 import DayJS from 'dayjs';
 import { useEffect, useState } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Blur } from '../components/blur';
 import { DoughnutChart } from '../components/charts/doughnut-chart';
+import { HorizontalChart } from '../components/charts/horizontal-chart';
+import { VerticalChart } from '../components/charts/vertical-chart';
 import { Budget } from '../components/dashboard/budget';
 import { LatestOrders } from '../components/dashboard/latest-orders';
 import { LatestProducts } from '../components/dashboard/latest-products';
@@ -226,6 +228,169 @@ const Dashboard = () => {
   // ------------------------------------------------------------------------------------
 
 
+  // Chart Helpers
+  const today = new Date();
+
+  // Date Helpers
+  // 7, 14, 21
+  const [selectedDays, setSelectedDays] = useState(7);
+
+  useEffect(() => {
+    if (selectedDays) { 
+      getGraphData();
+    }
+  }, [selectedDays])
+
+  const onChange = (event) => {
+    console.log(event.target)
+    setSelectedDays(event.target.value);
+  };
+
+  // Top X Customers contributing to Revenue Helpers
+  const [selectedCount, setSelectedCount] = useState(5);
+
+  useEffect(() => {
+    if (selectedCount) { 
+      getHorizontalGraphData1()
+    }
+  }, [selectedCount])
+
+
+  const handleChange = (event) => {
+    console.log(event.target)
+    setSelectedCount(event.target.value);
+  };
+
+  const DropDown = () => {
+    return(
+      <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Date Range</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedDays}
+            label="time range"
+            onChange={onChange}
+          >
+            <MenuItem value={7}>Last 7 days</MenuItem>
+            <MenuItem value={14}>Last 14 days</MenuItem>
+            <MenuItem value={21}>Last 21 days</MenuItem>
+          </Select>
+        </FormControl>
+    );
+  };
+
+  const Counter = () => {
+    return(
+      <TextField 
+        fullWidth
+        type="number"
+        inputProps={{ max: 20, min: 1 }}
+        sx={{ width: 100 }}
+        label="Counter"
+        margin="normal"
+        name="Counter"
+        onChange={handleChange}
+        value={selectedCount}
+      />
+    );
+  };
+
+  // Total Sales amount chart data
+  // This year vs Last year
+  const [graphDataThisYear, setGraphDataThisYear] = useState([]);
+  const [graphDataLastYear, setGraphDataLastYear] = useState([]);
+  const [dateLabels, setDateLabels] = useState([]);
+
+  const getGraphData = async () => {
+    const apiUrl = `${apiHost}/revenue/salesAmount`;
+    const body = JSON.stringify({
+      days: selectedDays,
+      currentDate: today,
+      organisationId: organisationId
+    });
+    const requestOptions = requestOptionsHelper('POST', body);
+
+   await fetch(apiUrl,requestOptions).then(res => res.json())
+    .then(result => {
+      const arrSalesAmountThisYear = []
+      const arrSalesAmountLastYear = []
+      const arrDateLabels = []
+
+      result.forEach(item => {
+        arrSalesAmountThisYear.push(item.salesAmountThisYear)
+        arrSalesAmountLastYear.push(item.salesAmountLastYear)
+        arrDateLabels.push(item.dateKey)
+      })
+
+      const reversedSalesThisYear = arrSalesAmountThisYear.reverse();
+      const reversedSalesLastYear = arrSalesAmountLastYear.reverse();
+      const reversedDays = arrDateLabels.reverse();
+
+      setGraphDataThisYear(reversedSalesThisYear);
+      setGraphDataLastYear(reversedSalesLastYear);
+      setDateLabels(reversedDays);
+    });
+  };
+
+  // horizontal graph data 1
+  // Top X revenue contributors
+  const [graphData1, setGraphData1] = useState([]);
+  const [graphLabels1, setGraphLabels1] = useState([]);
+
+  const getHorizontalGraphData1 = async () => {
+    const apiUrl = `${apiHost}/revenue/contributors`;
+    const body = JSON.stringify({
+      inDate: today,
+      start: null,
+      end: null,
+      range: false,
+      type: "month",
+      organisationId: organisationId,
+      count:selectedCount
+    });
+    const requestOptions = requestOptionsHelper('POST', body);
+    
+    await fetch(apiUrl,requestOptions).then(res => res.json())
+      .then(result => {
+        const nameLabels = [];
+        const contributionLabels = [];
+
+        result.forEach(item => {
+          nameLabels.push(item.name)
+          contributionLabels.push(item.contribution)
+        });
+
+        setGraphData1(contributionLabels);
+        setGraphLabels1(nameLabels);
+      });
+  };
+
+  // horizontal graph data 2
+  // 
+  const [graphData2, setGraphData2] = useState([]);
+  const [graphLabels2, setGraphLabels2] = useState([]);
+
+  const getHorizontalGraphData2 = async () => {
+    const apiUrl = `${apiHost}/final-goods/topSales/${organisationId}`;
+
+    await fetch(apiUrl).then(res => res.json())
+      .then(result => {
+        const nameLabels = [];
+        const quantityLabels = [];
+
+        result.forEach(item => {
+          nameLabels.push(item.name)
+          quantityLabels.push(item.quantity)
+        });
+
+        setGraphData2(quantityLabels);
+        setGraphLabels2(nameLabels);
+      });
+  };
+  // ------------------------------------------------------------------------------------
+
+
   // INIT
   useEffect(() => {
     Promise.all([
@@ -240,6 +405,10 @@ const Dashboard = () => {
       // Set 2
       getInventory(),
       getDeliveries(),
+
+      // Charts
+      // 
+      getHorizontalGraphData2(),
     ])
     .then(values => {
       // console.log(values);
@@ -313,6 +482,36 @@ const Dashboard = () => {
         <GenericBigCard
           headerProps={costBreakdownHeaderProps}
           content={<CostBreakdownCardContent />}
+        />
+      </Grid>
+
+      {/* CHARTS */}
+      <Grid item lg={8} md={12} xl={9} xs={12}>
+        <VerticalChart
+          dropDown={<DropDown/>}
+          dateLabels={dateLabels}
+          graphDataThisYear={graphDataThisYear}
+          graphDataLastYear={graphDataLastYear}
+          graphTitle= "Total Sales Amount"
+        />
+      </Grid>
+
+      <Grid item lg={6} md={12} xl={9} xs={12}>
+        <HorizontalChart 
+          graphLabels={graphLabels2}
+          graphData={graphData2}
+          graphTitle="Top 5 best selling goods this month"
+          sx={{ height: '100%' }} 
+        />
+      </Grid>
+
+      <Grid item lg={6} md={12} xl={9} xs={12}>
+        <HorizontalChart 
+          counter={<Counter/>}
+          graphLabels={graphLabels1}
+          graphData={graphData1}
+          graphTitle={`Top ${selectedCount} customers contributing to revenue`}
+          sx={{ height: '100%' }}
         />
       </Grid>
     </>
